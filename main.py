72cdabe850c7d4561c9a095d9ae1bd528782ce26
Vincent Elequin <117088893+GMVince@users.noreply.github.com>
2023-01-10 12:27:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# @TODO: Write a function that returns the arithmetic average for a list of numbers
+
+def average(numbers):
+    length = len(numbers)
+    total = 0.0
+    # for number in -numbers:
+    # total += number
+    total =sum(numbers)
+    return total / length
+
+
+print(average([1, 5, 9]))
+print(average(range(11)))
+
+# Test your function with the following:
+# print(average([1, 5, 9]))
+# print(average(range(11)))
